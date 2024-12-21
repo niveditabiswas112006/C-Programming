1b50db7734b712e7b20900c98c2b48189fd3d166
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:46:14 +0530

    Create Assignment-3.cpp

---
+++ Assignment-3.cpp
@@ -0,0 +1,14 @@
+// 3. W.A.P a program to swap 2 variable.
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 20;
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout<<"The value of a is: "<<a <<endl;
+    cout<<"The value of b is: "<<b <<endl;
+    return 0;
+}
