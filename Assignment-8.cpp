3449d9e608fa2bcd9110c5a4cdb5147eab320d6d
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:49:38 +0530

    Create Assignment-8.cpp

---
+++ Assignment-8.cpp
@@ -0,0 +1,14 @@
+// 8. W.A.P a program to store and display different data types.
+#include <iostream>
+using namespace std;
+int main(){
+    char ch = 'a';
+    double d = 5.6;
+    bool b = true;
+    int i = 10;
+    cout<<"character: "<<ch <<endl;
+    cout<<"double: "<<d <<endl;
+    cout<<"boolean: "<<(b? "true":"false")<<endl;
+    cout<<"integer: "<<i <<endl;
+    return 0;
+}
