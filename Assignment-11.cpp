e9865f8a92c8ea975ecd41608004e9b692cbf417
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:51:43 +0530

    Add files via upload

---
+++ Assignment-11.cpp
@@ -0,0 +1,8 @@
+// 11. W.A.P a program to store and display a string variable.
+#include <iostream>
+using namespace std;
+int main(){
+    string str = "hello world";
+    cout<<"string: "<<str <<endl;
+    return 0;
+}
