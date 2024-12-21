325a2670d63e3963dc050f018d77aa51d30e2cb9
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:59:02 +0530

    Add files via upload

---
+++ Assignment-41.cpp
@@ -0,0 +1,13 @@
+// Break statement:
+// code statement:
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i < 10; i ++){
+        if (i == 4){
+            break;
+        }
+        cout<<i<<"\n";
+    }
+    return 0;
+}
