66beb863fb9d3951ee7922b280fa848aef796168
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:57:16 +0530

    Add files via upload

---
+++ Assignment-37.cpp
@@ -0,0 +1,12 @@
+// While loop:
+// code statement:
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 0;
+    while (i < 5){
+        cout<<i <<"\n";
+        i++;
+    }
+    return 0;
+}
---
+++ Assignment-38.cpp
@@ -0,0 +1,12 @@
+// For loop:
+// code statement:
+#include <iostream>
+using namespace std;
+int main(){
+    int i = 0;{
+        for(int j = 0; j < 5; j ++){
+            cout<<j <<"\n";
+        }
+        return 0;
+    }
+}
