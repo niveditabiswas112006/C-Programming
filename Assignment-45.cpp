4cab3a587f08fec03efc21b7d0ed52ef319301cb
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-22 00:00:32 +0530

    Add files via upload

---
+++ Assignment-45.cpp
@@ -0,0 +1,10 @@
+// string user input.
+#include <iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+    cout<<"You entered: "<<str<<endl;
+    return 0;
+}
