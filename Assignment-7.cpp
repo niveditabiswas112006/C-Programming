48c82ab619a408f53198650623197d2409afd759
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:48:51 +0530

    Create Assignment-7.cpp

---
+++ Assignment-7.cpp
@@ -0,0 +1,12 @@
+// 7. W.A.P a program to find the sum of multiple variable. 
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+    int sum = a + b + c + d;
+    cout<<"The sum of a, b, c, d is: "<<sum <<endl;
+    return 0;
+}
