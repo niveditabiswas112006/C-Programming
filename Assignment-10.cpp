6840295debda7912273d503c268b2444ec1aad4c
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:50:58 +0530

    Create Assignment-10.cpp

---
+++ Assignment-10.cpp
@@ -0,0 +1,43 @@
+// 10. W.A.P a program to sum of 2 variable.
+#include <iostream>
+using namespace std;
+int main(){
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    cout<<"The sum of 2 variable is: "<<sum <<endl;
+    return 0;
+}
+
+// 10.1. W.A.P a program to sub of 2 variable.
+#include <iostream>
+using namespace std;
+int main(){
+    int num1 = 10;
+    int num2 = 20;
+    int sub = num1 - num2;
+    cout<<"The sub of 2 variable is: "<<sub <<endl;
+    return 0;
+}
+
+// 10.2. W.A.P a program to multiplication of 2 variable.
+#include <iostream>
+using namespace std;
+int main(){
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 * num2;
+    cout<<"The multiplication of 2 variable is: "<<sum <<endl;
+    return 0;
+}
+
+// 10.3. W.A.P a program to division of 2 variable.
+#include <iostream>
+using namespace std;
+int main(){
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 / num2;
+    cout<<"The div of 2 variable is: "<<sum <<endl;
+    return 0;
+}
