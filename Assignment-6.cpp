c868edfc93c22f7c5dbfca08e80f2faf6d9e9b27
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:48:19 +0530

    Create Assignment-6.cpp

---
+++ Assignment-6.cpp
@@ -0,0 +1,16 @@
+// 6. W.A.P a program to a simple arithmetic operations (addition, subtraction, multiplication, division).
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 20;
+    int addition = a + b;
+    int subtraction = a - b;
+    int multiplication = a * b;
+    int division = a / b;
+    cout<<"The addition of a and b is: "<<addition <<endl;
+    cout<<"The subtraction of a and b is: "<<subtraction <<endl;
+    cout<<"The multiplication of a and b is: "<<multiplication <<endl;
+    cout<<"The division of a and b is: "<<division <<endl;
+    return 0;
+}
