c8ee2ff2d0940efc4ef5996b1a715925185d8453
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-21 23:56:24 +0530

    Add files via upload

---
+++ Assignment-35.cpp
@@ -0,0 +1,14 @@
+// 35. W.A.P a program to declaring an multi dimensional array & printing.
+#include <iostream>
+using namespace std;
+int main(){
+    int arr [2][2] = {
+        {1,1},
+        {2,2}
+    };
+    cout<<arr[0][0];
+    cout<<arr[0][1]<<endl;
+    cout<<arr[1][0];
+    cout<<arr[1][1]<<endl;
+    return 0;
+}
