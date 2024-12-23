84a06c8811dcbee92b86fb686d3039c4df9000b0
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-23 10:37:47 +0530

    Add files via upload

---
+++ Assignment-49.cpp
@@ -0,0 +1,32 @@
+// 49. void display.
+// code statement.
+// #include <iostream>
+// using namespace std;
+// void display(int a);
+// int main(){
+//     int a;
+//     cout<<"enter the value of a is: ";
+//     cin>>a;
+//     int a = 10;
+//     display (a);
+//     return 0;
+// }
+// void display(int a){
+//     cout<<"value of a is: "<<a;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// void display(int a);
+// int main(){
+//     int x;
+//     cout<<"enter the value of x is: ";
+//     cin>>x;
+//     int x = 10;
+//     display (10);
+//     return 0;
+// }
+// void display(int a){
+//     cout<<"value of a is: "<<a;
+// }
---
+++ Assignment-50.cpp
@@ -0,0 +1,14 @@
+// 50. int function without void:
+// code statement:
+// #include <iostream>
+// using namespace std;
+// int display();
+// int main()
+// {
+//     cout<<display();
+//     return 0;
+// }
+// int display(){
+//     int x = 10;
+//     return x;
+// }
