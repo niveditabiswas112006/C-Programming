fe9cc39098f35a0707c599ca42b1b3938feffeaf
Nivedita Biswas <nivedita10012006@gmail.com>
2024-12-23 10:36:54 +0530

    Add files via upload

---
+++ Assignment-47.cpp
@@ -0,0 +1,22 @@
+// 47. Some Operation on C style string.
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char str1 [10] = "hello";
+//     char str2 [10] = "world";
+//     char str3 [10];
+    
+//     //copy str1 into str3.
+//     strcpy (str3, str1);
+//     cout<<"strcpy (str3, str1): "<<str3 <<endl;
+
+//     //concatenates str1 & str2.
+//     strcpy (str1, str2);
+//     cout<<"strcat (str1, str2): "<<str1 <<endl;
+
+//     //total length of strl after concatenation.
+//     int len = strlen (str1);
+//     cout << "strlen (str1): "<<len <<endl;
+//     return 0; 
+
+// }
---
+++ Assignment-48.cpp
@@ -0,0 +1,24 @@
+// 48. with parameter:
+// code statement:
+// #include <iostream>
+// using namespace std;
+// void display();
+// int main()
+// {
+//     cout<<"hello!";
+//     return 0;
+// }
+
+// without parameter:
+// code statement:
+// #include <iostream>
+// using namespace std;
+// void display (int a);
+// int main(){
+//     cout <<&display;
+//     return 0;
+// }
+// void display(int a)
+// {
+//     cout<<"value of a is: "<<a;
+// }
